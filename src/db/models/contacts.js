edd0a4abb2b498345508efe94cef7917134d4c68
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-06-07 15:48:59 +0200

    done

---
+++ src/db/models/contacts.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from 'mongoose';
+
+const ContactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const ContactsCollection = mongoose.model('contacts', ContactsSchema);
---
+++ src/server.js
@@ -0,0 +1,82 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { env } from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/contacts', async (req, res) => {
+    try {
+      const contacts = await getAllContacts();
+
+      res.status(200).json({
+        status: 'success',
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: 'Something went wrong',
+        error: err.message,
+      });
+    }
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
+
+      if (!contact) {
+        return res.status(404).json({
+          message: 'Contact not found',
+        });
+      }
+
+      res.status(200).json({
+        status: 'success',
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: 'Something went wrong',
+        error: err.message,
+      });
+    }
+  });
+
+  app.use('*', (req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.info(`Server is running on port ${PORT}`);
+  });
+};
+
+export default setupServer;
