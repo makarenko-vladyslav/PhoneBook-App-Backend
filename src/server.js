4ee0c9135a2fe9adee9149ca5a3387e7d7905f84
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-06-26 22:11:08 +0200

    all's done, only ROLES left

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import cookieParser from 'cookie-parser';
+
 import { env } from './utils/env.js';
-import contactsRouter from './routers/contacts.js';
+
+import router from './routers/index.js';
+
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
@@ -27,7 +31,9 @@ const setupServer = () => {
     }),
   );
 
-  app.use(contactsRouter);
+  app.use(cookieParser());
+
+  app.use(router);
 
   app.use('*', notFoundHandler);
   app.use(errorHandler);
